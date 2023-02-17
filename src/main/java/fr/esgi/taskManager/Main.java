98b5ea4924388e613ef373dd03ffb8aab0fdc84a
HLimam <heithem.limame@gmail.com>
2023-02-17 18:29:35 +0100

    add args

--- src/main/java/fr/esgi/taskManager/Main.java
+++ src/main/java/fr/esgi/taskManager/Main.java
@@ -1,7 +1,35 @@
 package fr.esgi.taskManager;
 
 public class Main {
+    private static String filename = System.getProperty("user.home") + "/.consoleagenda/log.txt";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length < 1) {
+            System.out.println("Error: No command specified.");
+            System.exit(1);
+        }
+
+        try {
+            switch (args[0]) {
+                case "list":
+                    listTasks();
+                    break;
+                case "add":
+                    addTask(args);
+                    break;
+                case "remove":
+                    removeTask(args);
+                    break;
+                case "update":
+                    updateTask(args);
+                    break;
+                default:
+                    System.out.println("Error: Invalid command.");
+                    System.exit(1);
+            }
+        } catch (IOException e) {
+            System.out.println("Error: Failed to write to log file.");
+            System.exit(1);
+        }
     }
 }
